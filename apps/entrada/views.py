e692a84b2f6de13164fd93e66daad1ec9804723c
Jefferson Silverio <jefferson.silverio@syngenta.com>
2023-04-27 15:41:36 -0300

    feat: Add notification for Django Messages in base.html

--- apps/entrada/views.py
+++ apps/entrada/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from apps.entrada.forms import EntradaForm
 from django.views.generic import ListView
 from apps.entrada.models import Entrada
 from django_filters.views import FilterView
 from django_filters import FilterSet, CharFilter, DateFilter
+from django.contrib import messages
+from django.shortcuts import redirect
 
 # class entradaFilter(FilterSet):
 #     class Meta:
@@ -16,9 +19,19 @@ class entrada(FilterView):
     model = Entrada
     context_object_name = "entradas"
     template_name = "entrada/entrada.html"
-    paginate_by = 3
+    paginate_by = 20
 
 
 def entrada_cadastro(request):
-    form = EntradaForm()
+    if request.method == "POST":
+        form = EntradaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Entrada salva com sucesso")
+            return HttpResponseRedirect(reverse("entrada_cadastro"))
+        else:
+            messages.error(request, "Erro ao salvar")
+    else:
+        form = EntradaForm()
+    
     return render(request, "entrada/entrada_cadastro.html", {'form': form})
