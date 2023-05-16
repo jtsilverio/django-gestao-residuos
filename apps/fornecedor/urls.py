d307733b883e8dc1ea148fad73ec9ff892d1b7b5
Mr-Jeffrey33 <90100773+Mr-Jeffrey33@users.noreply.github.com>
2023-05-15 21:23:50 -0300

    Alterações sobre a página de fornecedores
    Updates das páginas de fornecedor e iniciando a parte lógica da página de cadastro:
    criado botões de criação
    página de cadastro
    falta:
    criar a parte lógica para registrar o cadastro
    melhorar a apresentação dos campos na página

--- apps/fornecedor/urls.py
+++ apps/fornecedor/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
+from apps.fornecedor import views
 
 urlpatterns = [
     path("", views.fornecedor, name="fornecedor"),
     path("destinacao", views.destinacao, name="destinacao"),
+    path("fornecedor_cadastro.html", views.cadastro, name="fornecedor_cadastro")
 ]
