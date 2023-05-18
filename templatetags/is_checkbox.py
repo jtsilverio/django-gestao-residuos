7d78a25d5548f10075c1e3668d67f02bdf2251c6
Jefferson Silverio <jefferson.silverio@syngenta.com>
2023-05-18 08:37:55 -0300

    fix: changed the checkbox template tag to handle boolean fields in the front-end

--- templatetags/is_checkbox.py
+++ templatetags/is_checkbox.py
@@ -5,6 +5,6 @@ from django.forms import CheckboxInput, CheckboxSelectMultiple
 
 register = template.Library()
 
-@register.filter(name='is_checkbox')
-def is_checkbox(field):
-    return (isinstance(field.field.widget, CheckboxInput) or isinstance(field.field.widget, CheckboxSelectMultiple))
+@register.filter(name='is_multiple_choice')
+def is_multiple_choice(field):
+    return isinstance(field.field.widget, CheckboxSelectMultiple)
