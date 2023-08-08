ce237ac57c2b4e61f17c7aaa8eb1add96e343642
Jefferson Silverio <jefferson.silverio@syngenta.com>
2023-08-08 10:33:59 -0300

    feat: added fornecedor pages

--- apps/fornecedor/urls.py
+++ apps/fornecedor/urls.py
@@ -3,10 +3,20 @@ from django.urls import path
 from apps.fornecedor import views
 
 urlpatterns = [
-    path("", views.fornecedor, name="fornecedor"),
+    path("", views.fornecedor_index, name="fornecedor"),
     path(
         "fornecedor_cadastro.html",
-        views.fornecedor_cadastro,
+        views.FornecedorCreate.as_view(),
         name="fornecedor_cadastro",
+    ),
+    path(
+        "fornecedor_edit/<int:pk>",
+        views.FornecedorEdit.as_view(),
+        name="fornecedor_edit",
+    ),
+    path(
+        "fornecedor_delete/<int:pk>",
+        views.fornecedor_delete,
+        name="fornecedor_delete",
     ),
 ]
