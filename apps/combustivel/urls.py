07ad03ad19bafbb53ecd250442164078e8c4985b
Mr-Jeffrey33 <90100773+Mr-Jeffrey33@users.noreply.github.com>
2023-10-14 21:52:34 -0300

    branch_combustivel
    criado o branch para criação da página, porém quebrou uma pohada de coisas...
    ao tentar subir causa erro em vários locais

--- apps/combustivel/urls.py
+++ apps/combustivel/urls.py
@@ -4,4 +4,5 @@ from apps.combustivel import views
 
 urlpatterns = [
     path("", views.combustivel_index, name="combustivel"),
+    path("registro/", views.CombustivelReg.as_view(), name="combustivel_registro")
 ]
--- apps/combustivel/views.py
+++ apps/combustivel/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def combustivel_index(request):
+    query_paginated = Paginator(model_filter.qs PAGESIZE)
     return render(request, "combustivel/combustivel.html")
+
